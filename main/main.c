2a8000af49c31a170b3e77eafbc7f1934e887ce7
AnchisaPhetnoi <65030289@kmitl.ac.th>
2024-09-12 16:07:49 +0700

    first commit

---
+++ main/main.c
@@ -0,0 +1,44 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "freertos/semphr.h"
+#include "driver/gpio.h"
+
+#define LED_PIN 27
+#define PUSH_BUTTON_PIN 33
+
+TaskHandle_t ISR = NULL;
+
+void interrupt_task(void *arg)
+{
+    bool led_status = false;
+    while (1)
+    {
+        vTaskSuspend(NULL);
+        led_status = !led_status;
+        gpio_set_level(LED_PIN, led_status);
+        printf("Button pressed!\n");
+    }
+}
+
+void IRAM_ATTR button_isr_handler(void *arg)
+{
+    xTaskResumeFromISR(ISR);
+}
+
+void app_main(void)
+{
+    esp_rom_gpio_pad_select_gpio(PUSH_BUTTON_PIN);
+   esp_rom_gpio_pad_select_gpio(LED_PIN);
+
+    gpio_set_direction(PUSH_BUTTON_PIN, GPIO_MODE_INPUT);
+    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+
+    gpio_set_intr_type(PUSH_BUTTON_PIN, GPIO_INTR_POSEDGE);
+
+    gpio_install_isr_service(0);
+
+    gpio_isr_handler_add(PUSH_BUTTON_PIN, button_isr_handler, NULL);
+
+    xTaskCreate(interrupt_task, "interrupt_task", 4096, NULL, 10, &ISR);
+}
